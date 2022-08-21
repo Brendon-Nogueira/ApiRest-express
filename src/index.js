56d95c1b947b796e6f745cb75aedeb3d5c4cc104
Brendon-Nogueira <brendonnogueira21@gmail.com>
2022-08-21 11:00:27 -0300

    criando objeto

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express') 
+
+
+// fake data base
+let courses = []
+
+
+//app 
+const app = express
+
+app.use(express.json())
+
+app.post(`/${courses}`, (req,res)=>{
+    const {name, course, year} = req.body
+    const curso = {name, course,year}
+    courses.push(curso)
+    res.status(201).json(curso)
+})
+
+
+//servidor rodando 
+app.listen(8080, ()=> console.log('Server funcionando'))
