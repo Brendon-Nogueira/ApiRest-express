2ce42a545b6d86f84a1426e38549e9a6d75758b5
Brendon-Nogueira <brendonnogueira21@gmail.com>
2022-08-21 16:33:13 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
 const express = require('express') 
+const moment = require('moment')
 
 // fake data base
-let courses = []
-
+let schedule = {
+    'year' : 0,
+    'semester' : 0,
+    'days' : [1,2,3,4,5],
+}
 
 //app 
 const app = express()
-
 app.use(express.json())
 
 //callback
-app.post('/courses', (req,res)=>{
-    const {name, course, year} = req.body
-    const curso = {name, course,year}
-    courses.push(curso)
-    return res.status(201).json(curso)
+app.post('/api/v1/classes', (req,res)=>{
+    const {year, semester, days} = req.body
+    const weekly = {year,semester,days}
+
+    if (weekly.days < 1 || weekly.days > 5){
+        console.log('Valor de data incorreto')
+        return res.status(400).end()
+    } else if (weekly.semester !== 1 && weekly.semester !== 2) {
+        console.log('Valor de semestre incorreto')
+        return res.status(400).end()
+    } else {
+        schedule.push(weekly)
+        return res.status(201).json(weekly)
+    }
 })
 
-app.get('/courses', (req, res) =>{
-    const allCourses = courses
-    console.log('lista de cursos', `${allCourses}`)
-    return res.status(200).json(allCourses)
+app.get('/api/v1/classes', (req, res) =>{
+    const allDays = schedule
+    console.log(`lista de datas ${allDays}`)
+    return res.status(200).json(allDays)
 
 })
-
 
 //servidor rodando 
 app.listen(8080, ()=> console.log('Server funcionando'))
