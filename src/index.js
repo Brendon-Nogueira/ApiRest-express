d2f3445b75d670acba38ddc5d6b4f4225ceae86c
Brendon-Nogueira <brendonnogueira21@gmail.com>
2022-08-21 15:50:13 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 const express = require('express') 
 
-
 // fake data base
 let courses = []
 
 
 //app 
-const app = express
+const app = express()
 
 app.use(express.json())
 
-app.post('courses', (req,res)=>{
+//callback
+app.post('/courses', (req,res)=>{
     const {name, course, year} = req.body
     const curso = {name, course,year}
     courses.push(curso)
     return res.status(201).json(curso)
+})
+
+app.get('/courses', (req, res) =>{
+    const allCourses = courses
+    console.log('lista de cursos', `${allCourses}`)
+    return res.status(200).json(allCourses)
+
 })
 
 
