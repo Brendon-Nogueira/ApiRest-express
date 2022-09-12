bab8918fd14fcb647759955138116ca9c99dfd84
Brendon-Nogueira <brendonnogueira21@gmail.com>
2022-09-11 23:22:57 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,44 +1,83 @@
-const express = require('express') 
+const express = require('express')
 const moment = require('moment')
 
-// fake data base
-let schedule = {
-    'year' : 0,
-    'semester' : 0,
-    'days' : [1,2,3,4,5],
-}
+const HOST = '0.0.0.0'
+
+//fake db
+
+let daysList = []
 
 //app 
 const app = express()
 app.use(express.json())
 
 //callback
-app.post('/api/v1/classes', (req,res)=>{
-    const {year, semester, days} = req.body
-    const weekly = {year,semester,days}
 
-    if (weekly.days < 1 || weekly.days > 5){
-        console.log('Valor de data incorreto')
-        return res.status(400).end()
-    } else if (weekly.semester !== 1 && weekly.semester !== 2) {
-        console.log('Valor de semestre incorreto')
-        return res.status(400).end()
+app.get('/api/v1/classes', (req, res) => {
+    res.send(daysList)
+})
+
+app.post('/api/v1/classes', (req, res) => {
+    const postData = req.body
+    const isValid = makeData(postData)
+
+    res.send(daysList)
+    if(isValid.error){
+        res.status(400).send(isValid)
     } else {
-        schedule.push(weekly)
-        return res.status(201).json(weekly)
+        res.status(200).json(makeData)
     }
-})
-
-let makeData = () => {
     
-}
-
-app.get('/api/v1/classes', (req, res) =>{
-    const allDays = schedule
-    console.log(`lista de datas ${allDays}`)
-    return res.status(200).json(allDays)
-
 })
 
-//servidor rodando 
-app.listen(8080, ()=> console.log('Server funcionando na porta 8080'))
+
+function makeData(postData) {
+    const {year, semester, days} = postData
+    if (semester == 1) {  
+        let data = moment([year, 1])
+        let finalData = moment([year, 1]).add(5, "month").add(-1, "days")
+        let tmpDay = data
+    
+        for (let j = 0;;j++) {
+            //Verifica se o dia é anterior ao último dia do semestre
+            if (tmpDay.isBefore(finalData)) {
+                //Verifica quando é a primeira posição para não adicionar o próximo dia
+                if (j !== 0) {
+                    tmpDay.add(1, "day")
+                }
+                for(let i = 0; i < days.length;i++) {
+                    //Verifica se o dia é o mesmo que o informado naquela posição do array
+                    if(tmpDay.days() === days[i]) {
+                        daysList.push(tmpDay.format('DD/MM/YYYY'))
+                    }
+                }
+            } else {
+                break
+            }
+        }
+    } else if (semester == 2) {
+        let data = moment([year, 7])
+        let finalData = moment([year, 7]).add(4, "month").add(-1, "days")
+        console.log(finalData.format('DD/MM/YYYY'))
+        let tmpDay = data
+
+        for (let j = 0;;j++) {
+            //Verifica se o dia é anterior ao último dia do semestre
+            if (tmpDay.isBefore(finalData)) {
+                //Verifica quando é a primeira posição para não adicionar o próximo dia
+                if (j !== 0) {
+                    tmpDay.add(1, "day")
+                }
+                for(let i = 0; i < days.length;i++) {
+                    //Verifica se o dia é o mesmo que o informado naquela posição do array
+                    if(tmpDay.days() === days[i]) {
+                        daysList.push(tmpDay.format('DD/MM/YYYY'))
+                    }
+                }
+            } else {
+                break
+            }
+        }
+        
+    }     
+}
